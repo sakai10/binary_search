6550fc774aa26e135a8775f924d8faf2a314d65b
sakai taiga <tai0201shake@gmail.com>
2020-07-07 11:43:32 +0900

    revised

--- src/apple.c
+++ src/apple.c
@@ -4,14 +4,37 @@ int n;
 int k;
 int A[100000];
 
-
+int p(int x){
+  int y=0;
+  for(int j = 0; j < n; j++){
+    y += (A[j]+x-1)/x;
+  }
+  return y <= k;
+}
 int main(){
   int i, lb, ub;
   scanf("%d%d", &n, &k);
   for(i = 0; i < n; i++){
     scanf("%d", &A[i]);
   }
+  
+  int a_max = 0;
+  for(int k = 0; k < n; k++){
+      if(A[k] > a_max) a_max = A[k];
+    }
 
-
+  lb  = 0;
+  ub = a_max + 1;
+  while(ub - lb > 1){
+      int mid = (lb + ub) / 2;
+      if(p(mid)){
+          ub = mid;
+      }
+      else{
+          lb = mid;
+      }
+   }
+   
+  printf("%d\n", ub);
   return 0;
 }
